29d9559a91139477ab2f23cb3e99f222aebf2ad5
John A. Thywissen <jthywiss@users.noreply.github.com>
2017-11-01 08:34:26 -0500

    Automate HDFS runs with varying number of files

--- tools/run-holmes.sh
+++ tools/run-holmes.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+run_one () {
+  echo 'hdfs dfs -rm -r output\*'
+  hdfs dfs -rm -r output\*
+  echo
+  echo
+  echo -n 'File name prefix: '
+  export orc_executionlog_fileprefix=HolmesWordCountHadoop_$(eval hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.GetFactorString -libjars ${HADOOP_CLASSPATH//:/,} "$@")
+  echo ${orc_executionlog_fileprefix}
+  echo
+  echo
+  echo "hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.HolmesWordCountHadoop -libjars ..." "$@" "output"
+  # { { eval hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.HolmesWordCountHadoop -libjars ${HADOOP_CLASSPATH//:/,} "$@" output | tee raw-output/${orc_executionlog_fileprefix}.out; exit ${PIPESTATUS[0]}; } 2>&1 1>&3 | tee raw-output/${orc_executionlog_fileprefix}.err; exit ${PIPESTATUS[0]}; } 3>&1 1>&2
+  { { eval hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.HolmesWordCountHadoop -libjars ${HADOOP_CLASSPATH//:/,} "$@" output | tee raw-output/${orc_executionlog_fileprefix}.out; } 2>&1 1>&3 | tee raw-output/${orc_executionlog_fileprefix}.err; } 3>&1 1>&2
+  echo
+  echo
+  echo 'hdfs dfs -cat output\*/\*'
+  hdfs dfs -cat output\*/\*
+  echo
+  echo
+}
+
 export ORC_TEST_ROOT=/u/${LOGNAME}/orc-test-root
 export HADOOP_CLASSPATH=${ORC_TEST_ROOT}/OrcScala/build/orc-2.1.1.jar:${ORC_TEST_ROOT}/OrcScala/lib/scala-library.jar:${ORC_TEST_ROOT}/OrcScala/lib/scala-parser-combinators_2.12-1.0.6.jar:${ORC_TEST_ROOT}/OrcScala/lib/scala-reflect.jar:${ORC_TEST_ROOT}/OrcScala/lib/scala-xml_2.12-1.0.6.jar:${ORC_TEST_ROOT}/PorcE/build/classes/:${ORC_TEST_ROOT}/OrcTests/build/
-#FIXME: compute this file name prefix
-export orc_executionlog_fileprefix=HolmesWordCountHadoop_120_6
 
 # Hadoop requires that the class path entries are world readable
 echo 'Making class path entries world readable'
@@ -31,17 +50,12 @@ echo 'rm raw-output/*'
 rm raw-output/*
 echo
 echo
-echo 'hdfs dfs -rm -r output\*'
-hdfs dfs -rm -r output\*
-echo
-echo
 echo 'slaves.sh uptime'
 slaves.sh uptime
 echo
 echo
-echo 'hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.HolmesWordCountHadoop -libjars ... input output'
-{ { hadoop jar dOrc-Hadoop-experiments.jar orc.test.item.distrib.HolmesWordCountHadoop -libjars ${HADOOP_CLASSPATH//:/,} input output | tee raw-output/${orc_executionlog_fileprefix}.out; exit ${PIPESTATUS[0]}; } 2>&1 1>&3 | tee raw-output/${orc_executionlog_fileprefix}.err; exit ${PIPESTATUS[0]}; } 3>&1 1>&2
-echo
-echo
-echo 'hdfs dfs -cat output\*/\*'
-hdfs dfs -cat output\*/\*
+for input_files in "input/input-copy-{1..24}.txt" "input/input-copy-{1..48}.txt" "input/input-copy-{1..72}.txt" "input/input-copy-{1..96}.txt" "input/input-copy-{1..120}.txt"
+do
+  run_one ${input_files}
+done
+echo 'Runs complete.'
